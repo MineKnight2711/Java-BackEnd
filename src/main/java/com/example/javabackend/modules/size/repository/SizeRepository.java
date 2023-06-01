cc364c4279234bf7fbc1d575c2fd77cf852d51d3
Nguyễn Quý <nguyentrongquy612@gmail.com>
2023-06-01 10:49:59 +0700

    change log in response

--- src/main/java/com/example/javabackend/modules/size/repository/SizeRepository.java
+++ src/main/java/com/example/javabackend/modules/size/repository/SizeRepository.java
@@ -1,4 +1,12 @@
 package com.example.javabackend.modules.size.repository;
 
-public class SizeRepository {
+import com.example.javabackend.entity.Size;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SizeRepository extends JpaRepository<Size, Long> {
+    @Query("SELECT u FROM Size u where u.SizeID = ?1")
+    Size findById(String id);
 }
